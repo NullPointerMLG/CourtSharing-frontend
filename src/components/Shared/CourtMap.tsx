028eac45256bb0110866c7a7e11333d11833325f
JaysusM <jmartinruiz@uma.es>
2019-12-01 17:44:21 +0100

    :construction: Add fab button in Map when event is clicked

--- src/components/Shared/CourtMap.tsx
+++ src/components/Shared/CourtMap.tsx
@@ -1,15 +1,35 @@
 import React, { useContext, useState, useEffect } from "react";
 import { GeoJsonObject } from "geojson";
-import { Map, TileLayer, GeoJSON, Popup } from "react-leaflet";
-import L, { LatLngExpression, icon, Layer, IconOptions, Icon } from "leaflet";
-import { makeStyles, Theme } from "@material-ui/core";
+import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import L, {
+  LatLngExpression,
+  icon,
+  Layer,
+  IconOptions,
+  Icon,
+  Content
+} from "leaflet";
+import { makeStyles, Theme, Fab } from "@material-ui/core";
 import { SelectedSportContext } from "../../context/SportsContext";
 import { getCourts } from "../../services/API";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyle = makeStyles((theme: Theme) => ({
-  map: {
+  mapContainer: {
     margin: "0px 50px 50px 50px",
-    width: "100%"
+    width: "100%",
+    position: "relative"
+  },
+  addFab: {
+    position: "absolute",
+    margin: "10px",
+    bottom: "0px",
+    zIndex: 999999,
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    "&:hover": {
+      backgroundColor: theme.palette.primary.light
+    }
   }
 }));
 
@@ -18,6 +38,7 @@ export const CourtMap = () => {
   const position: LatLngExpression = [36.72354892, -4.427047];
   const [selectedSport] = useContext(SelectedSportContext);
   const [geoJson, setGeoJson] = useState<GeoJsonObject>();
+  const [selectedMarker, setSelectedMarker] = useState<LatLngExpression>();
   const sportIcon: Icon<IconOptions> | undefined = selectedSport
     ? icon({
         iconUrl: selectedSport.marker_url,
@@ -27,14 +48,22 @@ export const CourtMap = () => {
 
   useEffect(() => {
     getCourts(selectedSport._id.$oid)
-    .then(res => setGeoJson(res))
-    .catch(e => console.warn(e));
-  }, [selectedSport])
-
+      .then(res => setGeoJson(res))
+      .catch(e => console.warn(e));
+  }, [selectedSport]);
 
   const onEachFeature = (feature: any, layer: Layer) => {
     if (feature.properties) {
-      layer.bindPopup(feature.properties.NOMBRE);
+      const popup: Content = feature.properties.NOMBRE;
+      layer.addEventListener("popupopen", () => {
+        const coordinates = feature.geometry.coordinates;
+        const latLng: LatLngExpression = [coordinates[0], coordinates[1]];
+        setSelectedMarker(latLng);
+      });
+      layer.addEventListener("popupclose", () => {
+        setSelectedMarker(undefined);
+      });
+      layer.bindPopup(popup);
     }
   };
 
@@ -44,9 +73,13 @@ export const CourtMap = () => {
 
   return (
     <div>
-      {" "}
       {geoJson && selectedSport && (
-        <div className={classes.map}>
+        <div className={classes.mapContainer}>
+          {selectedMarker && (
+            <Fab className={classes.addFab}>
+              <AddIcon />
+            </Fab>
+          )}
           <Map center={position} zoom={13}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -61,7 +94,7 @@ export const CourtMap = () => {
             />
           </Map>
         </div>
-      )}{" "}
+      )}
     </div>
   );
 };
