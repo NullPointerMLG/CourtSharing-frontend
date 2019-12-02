4d87d7547170fe4edc7e30c68abc6407bf9f1355
JaysusM <jmartinruiz@uma.es>
2019-12-02 23:41:30 +0100

    :sparkles: Add parking filters

--- src/models/ParkingResponse.ts
+++ src/models/ParkingResponse.ts
@@ -3,5 +3,6 @@ import { GeoJsonObject } from 'geojson';
 export interface ParkingResponse {
     type: string,
     marker_url: string,
-    data: GeoJsonObject
+    data: GeoJsonObject,
+    active?: boolean
 }
