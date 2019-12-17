37e0a3c0d53663c3653f9fd3e35ee2d6cda21926
JaysusM <jmartinruiz@uma.es>
2019-12-17 16:29:58 +0100

    :alien: Change API url to heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ import { Event } from "../models/Event";
 import { Sport } from "../models/Sport";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-const BASE_URL: string = "http://localhost:5000";
+const BASE_URL: string = "http://courtsharing-api.herokuapp.com";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL
